ca618348cd838e017ccc05fee52ec1159d281d3f
GuiomP31 <guipast31@gmail.com>
2023-04-05 18:22:23 +0200

    creation prjet

---
+++ script.js
@@ -0,0 +1,46 @@
+// Script JS
+
+// ******************************************** Afficher liste des articles
+
+function masquer_tab(id) {
+    if (document.getElementById(id).style.display == 'none') {
+        document.getElementById(id).style.display = 'block';
+    }
+    else {
+        document.getElementById(id).style.display = 'none';
+    }
+}
+
+
+
+// ******************************************** Afficher liste des articles
+
+
+
+// ******************************************** Validation Formulaire
+let loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let nom = document.getElementById("nom");
+    let adress = document.getElementById("adress");
+    let tel = document.getElementById("tel");
+    let email = document.getElementById("email");
+
+    if (nom.value == "" || adress.value == "" || tel.value == "" || email.value == "") {
+        alert("Veillez à saisir une valeur correcte dans les quatre champs !");
+    } else {
+        // effectuer une opération à partir d'un formulaire
+        alert("Le formulaire a été envoyé avec succès !");
+        console.log(" Félicitations pour avoir envoyé votre commande !");
+        console.log(`Ce formulaire a comme nom d'utilisateur ${nom.value}, son adresse est ${adress.value},
+        son numéro de téléphone ${tel.value}, et son email ${email.value}`);
+        nom.value = "";
+        adress.value = "";
+        tel.value = "";
+        email.value = "";
+    }
+});
+
+// ******************************************** Validation Formulaire
